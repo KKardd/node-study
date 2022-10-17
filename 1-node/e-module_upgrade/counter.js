ee51c6eb942df47a03863510939ac9fcfedb2461
KKardd <wlstjd4013@gmail.com>
2022-10-17 18:11:42 +0900

    feat : module_upgrade

---
+++ 1-node/e-module_upgrade/counter.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+export function increase(){
+    count++;
+}
+
+export function getCount(){
+    return count;
+}
+
+// module.exports.getCount = getCount;
+// module.exports.increase = increase;
