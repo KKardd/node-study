3131d42385823f4f4d2d6b18f9f38ea37b58696c
KKardd <wlstjd4013@gmail.com>
2022-10-17 20:43:44 +0900

    feat : process

---
+++ 1-node/g-process/app.js
@@ -0,0 +1,19 @@
+const process = require('process');
+
+console.log(process.pid);
+console.log(process.cwd());
+console.log(process.uptime());
+console.log(process.cpuUsage());
+
+setTimeout(() => {
+    console.log('Time Out!');
+}, 0);
+
+process.nextTick(() => {
+    console.log('nextTick!');
+})
+// nextTick은 task queue에 있는 콜백함수를 무시하고 먼저 실행된다.
+
+for(let i = 0; i < 1000; i++){
+    console.log(i);
+} // 시간 지체
