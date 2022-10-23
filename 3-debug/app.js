39b20236e64598ea3f2b41d6e8937a5724c3c761
KKardd <wlstjd4013@gmail.com>
2022-10-23 23:57:13 +0900

    feat : debug

---
+++ 3-debug/app.js
@@ -0,0 +1,21 @@
+function sayHello(){
+    console.log('Hello!😁');
+}
+
+function calculator(x, y){
+    const result = x + y;
+    console.log('결과 : ', result);
+    sayHello();
+    return result;
+}
+
+calculator(1,2);
+
+const stop = 4;
+for(let i = 0; i < 10; i++){
+    console.log('looping.......');
+    console.log('count : ' , i);
+    if(i === stop){
+        break;
+    }
+}
