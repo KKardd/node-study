17692345943ac54c1ba8de23567b1c61ccf42444
KKardd <wlstjd4013@gmail.com>
2023-05-11 20:43:23 +0900

    modify api

--- server_express/router/board_Router.js
+++ server_express/router/board_Router.js
@@ -5,21 +5,23 @@ const router = express.Router();
 
 // GET /board
 // GET /board?username=:username
-router.get("/", isAuth, boardController.getTweets);
+router.get("/", boardController.getPostings);
 
 // GET /board/:id
 // router.get("/:id", isAuth, boardController.getTweet);
+app.get("/:id", boardController.getPostings);
 
 // POST /board
-router.post("/", isAuth, validateTweet, boardController.createTweet);
+// router.post("/", isAuth, validateTweet, boardController.createTweet);
+app.post("/post", boardController.newPostings);
 
 // PUT /board/:id
-router.put("/:id", isAuth, validateTweet, boardController.updateTweet);
+// router.put("/:id", isAuth, validateTweet, boardController.updateTweet);
 
 // DELETE /board/:id
-router.delete("/:id", isAuth, boardController.deleteTweet);
+// router.delete("/:id", isAuth, boardController.deleteTweet);
 
 // DELETE /board?username=:username
-router.delete("/", isAuth, boardController.deleteTweetByUserId);
+// router.delete("/", isAuth, boardController.deleteTweetByUserId);
 
 export default router;
