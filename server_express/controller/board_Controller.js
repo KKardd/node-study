437181ef2aece778788225a514752b0e1ac611d9
KKardd <wlstjd4013@gmail.com>
2023-05-14 20:50:29 +0900

    style fix

--- server_express/controller/board_Controller.js
+++ server_express/controller/board_Controller.js
@@ -2,7 +2,8 @@ import * as boardRepository from "../data/board_Data.js";
 
 export async function getPostings(req, res) {
     const username = req.query.username;
-    res.status(200).json(username);
+    const data = await (username ? boardRepository.getAllByUsername(username) : boardRepository.getAll());
+    res.status(200).json(data);
 }
 
 export async function newPostings(req, res) {
