25b137b4774b196b842d4ec1eaddc9cda15b3f88
KKardd <wlstjd4013@gmail.com>
2023-05-15 20:07:51 +0900

    fix getPosting function

--- server_express/controller/board_Controller.js
+++ server_express/controller/board_Controller.js
@@ -3,12 +3,13 @@ import * as boardRepository from "../data/board_Data.js";
 export async function getPostings(req, res) {
     const username = req.query.username;
     const data = await (username ? boardRepository.getAllByUsername(username) : boardRepository.getAll());
+    console.log(data);
     res.status(200).json(data);
 }
 
 export async function getPosting(req, res) {
     const id = req.params.id;
-    const data = boardRepository.getById(id);
+    const data = await boardRepository.getById(id);
     if (!data) {
         res.status(404).json(id);
     }
